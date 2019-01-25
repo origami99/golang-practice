31dc079b369eb61fbefa0c58343da21a723f8f67
Nikolay Lubomirov <origamista99@gmail.com>
2019-01-25 22:39:40 +0200

    Automation tested more details about the newDeck

--- DeckOfCardsApp/deck_test.go
+++ DeckOfCardsApp/deck_test.go
@@ -5,9 +5,21 @@ import "testing"
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
-	expectedDeckLen := 52
-	actualDeckLen := len(d)
-	if actualDeckLen != expectedDeckLen {
-		t.Errorf("Expected deck length of ", expectedDeckLen, " but got ", actualDeckLen)
+	deckLenExpected := 52
+	deckLenActual := len(d)
+	if deckLenExpected != deckLenActual {
+		t.Errorf("Expected deck length of %v but got %v", deckLenExpected, deckLenActual)
+	}
+
+	firstCardExpected := "Ace of Spades"
+	firstCardActual := d[0]
+	if firstCardExpected != firstCardActual {
+		t.Errorf("Expected first card of %v but got %v", firstCardExpected, firstCardActual)
+	}
+
+	lastCardExpected := "King of Clubs"
+	lastCardActual := d[len(d)-1]
+	if lastCardExpected != lastCardActual {
+		t.Errorf("Expected first card of %v but got %v", lastCardExpected, lastCardActual)
 	}
 }
